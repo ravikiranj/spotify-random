905b0c1936e6da016e801332b84bd634124cc08b
Ravikiran Janardhana <rjanardhana@tripadvisor.com>
2013-10-22 09:33:17 -0400

    Added random seed

--- sp.py
+++ sp.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-from random import randint
+from random import randint, seed
 
 # max artists
 maxA = 3
 # load file
-aFile = open("artists.txt", "r")
+aFile = open("/home/rjanardhana/git/spotify-random/artists.txt", "r")
 artists = aFile.read().split("\n")
 # strip white spaces
 artists = [a.strip() for a in artists if a.strip()]
 aLen = len(artists)
 count = 1
+seed()
 for i in xrange(maxA):
     # print artist
     j = randint(0, aLen-1)
